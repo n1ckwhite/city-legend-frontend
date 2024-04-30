0cf3c158537043876f98f2def79bcb46b1451433
nickhwite <bbyicnka@yandex.ru>
2024-04-30 13:40:24 +0300

    feat(main): Получение карт

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,17 @@
-export const App = () => (
-  <div>
-    <p>123</p>
-  </div>
-);
+import { useEffect } from "react";
+
+import { useAppDispatch } from "@/hooks/AppDispatch.ts";
+import { getCartsThunk } from "@/service/slices/carts.slice.ts";
+
+export const App = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getCartsThunk());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <p>123</p>
+    </div>
+  );
+};
---
+++ src/types/cart.interface.ts
@@ -0,0 +1,6 @@
+export interface ICart {
+  id: string;
+  name: string;
+  date: string;
+  presents: number;
+}
